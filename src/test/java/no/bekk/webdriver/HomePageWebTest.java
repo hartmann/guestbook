ae4796de67d973c91d34b7fca1910583c5d0eb65
hartmann <vegard.hartmann@gmail.com>
2010-07-04 11:44:34 +0200

    satt opp kjoring av ytelsestest via mvn

--- src/test/java/no/bekk/webdriver/HomePageWebTest.java
+++ src/test/java/no/bekk/webdriver/HomePageWebTest.java
@@ -1,5 +1,10 @@
 package no.bekk.webdriver;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Vegard Hartmann
@@ -8,4 +13,15 @@ package no.bekk.webdriver;
  * To change this template use File | Settings | File Templates.
  */
 public class HomePageWebTest {
+
+    public HomePageWebTest() {
+        System.out.println("Opprettet instans av klassen");
+    }
+
+    @Test
+    public void testThatApplicationIsUpAndRunning() {
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8080/guestbook/");
+        driver.quit();
+    }
 }
